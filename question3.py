24ba98d7da67a067b9ba4cdb0a95652baf6a246b
reignnnnn9 <reigne50@gmail.com>
2025-08-04 20:50:49 -0500

    pet_age update

--- question3.py
+++ question3.py
@@ -1,27 +1,28 @@
 #Pet Age Calculator
 
-type = input("Enter pet type (dog/cat/bird/hamster): ").capitalize()
-age = (input("Enter your pet's age in human years: "))
-if type == "dog":
+type = input("Enter pet type (dog/cat/bird/hamster): ").capitalize() #after this the type need to be capitalized
+age = int(input("Enter your pet's age in human years: "))
+pet_age = 0
+if type == "Dog":
     if age <= 2:
         pet_age = age * 12
     else:
         pet_age = 24 + (age - 2) * 4
-elif type == "cat":
+elif type == "Cat":
     if age <= 2:
         pet_age = age * 12
     else:
         pet_age = 24 + (age - 2) * 4
-elif type == "bird":
+elif type == "Bird":
     pet_age = age * 9
-elif type == "hamster":
+elif type == "Hamster":
     pet_age = age * 25
 print("\n")
 
 print("=== PET AGE CONVERSTION ===")
 print(f"Pet type: {type}")
 print(f"Human Age: {age} years")
-print(f"Pet Age: {pet_age} pet years") #can't figure out how to use the pet_age inside if statement in here.
+print(f"Pet Age: {pet_age} pet years") 
 print("\n")
 
 print(f"Fun fact: your {type} is like a {pet_age}-year-old human!")
